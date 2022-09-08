c43682ce633576263d584855bd2541967984e789
Light2288 <davide.aliti@gmail.com>
2022-09-08 18:11:35 +0200

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Davide Aliti on 08/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Davide Aliti on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
